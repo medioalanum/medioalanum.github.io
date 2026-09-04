639c23c00c54bc3a1a61c38ad0fa47b27bd7e2d1
Alan Viana <312699414+medioalanum@users.noreply.github.com>
2026-09-04 07:45:29 +0200

    Add Dracula syntax highlighting

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,6 +16,14 @@ HIDE_DATE = False
 DEFAULT_PAGINATION = 10
 RELATIVE_URLS = True
 
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+    },
+    "output_format": "html5",
+}
+
 ARTICLE_URL = "{slug}/"
 ARTICLE_SAVE_AS = "{slug}/index.html"
 PAGE_URL = "pages/{slug}/"
